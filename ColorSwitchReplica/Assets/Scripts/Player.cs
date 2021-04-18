0a3391be97b79839a7af90979e6d61bb03d0f53a
ihtiyarsoft <ihtiyaryasingurkan@gmail.com>
2021-04-19 01:14:54 +0300

    Example Project

---
+++ ColorSwitchReplica/Assets/Scripts/Player.cs
@@ -0,0 +1,69 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Player : MonoBehaviour
+{
+    [SerializeField] float jumpForce = 10f;
+
+    public Rigidbody2D rb;
+
+    [SerializeField] public SpriteRenderer sr;
+
+    [SerializeField] public string currentColour;
+
+    [SerializeField] public Color colorCyan;
+    [SerializeField] public Color colorPink;
+    [SerializeField] public Color colorPurple;
+    [SerializeField] public Color colorYellow;
+
+    void Start()
+    {
+        SetRandomColour();
+            
+    }
+
+
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            rb.velocity = Vector2.up * jumpForce;
+        }
+    }
+    void OnTriggerEnter2D(Collider2D col)
+    {
+        if( col.tag == "colorChanger")
+        {
+            SetRandomColour();
+            Destroy(col.gameObject);
+            return;
+        }
+        if( col.tag != currentColour)
+        {
+            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+        }
+    }
+    void SetRandomColour()
+    {
+        int index = Random.Range(0, 4);
+        switch (index)
+        {
+            case 0:
+                currentColour = "Cyan";
+                sr.color = colorCyan;
+                break;
+            case 1:
+                currentColour = "Yellow";
+                sr.color = colorYellow;
+                break;
+            case 2:
+                currentColour = "Purple";
+                sr.color = colorPurple;
+                break;
+            case 3:
+                currentColour = "Pink";
+                sr.color = colorPink;
+                break;
+        }
+    }
+}
---
+++ ColorSwitchReplica/Assets/Scripts/followPlayer.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+
+public class followPlayer : MonoBehaviour
+{
+    public Transform player;
+
+    void Update()
+    {
+        if (player.position.y > transform.position.y)
+        {
+            transform.position = new Vector3(transform.position.x, player.position.y, transform.position.z);
+        }
+    }
+}
